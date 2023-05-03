f8c4e36cd4f085c79405c2ebda87a268ca8af23c
danahossini <danahossini1@gmail.com>
2023-05-04 01:02:42 +0430

    set env

--- next.config.js
+++ next.config.js
@@ -2,15 +2,7 @@ require('dotenv-safe').config();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-    API_KEY: process.env.API_KEY,
-    GITHUB_ID: process.env.GITHUB_ID,
-    GITHUB_SECRET: process.env.GITHUB_SECRET,
-    JWT_KEY: process.env.JWT_KEY,
-    ADMIN1: process.env.ADMIN1,
-    ADMIN2: process.env.ADMIN2
-  },
+
   reactStrictMode: true,
 }
 
